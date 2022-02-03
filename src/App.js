cdcf8e79d96af7344e493cc1d1d52e538eeb7c93
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-03 12:32:48 -0700

    created initial async and useeffect in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getDrums } from './services/fetch-utils';
 
 function App() {
+
+  const [drums, setDrums] = useState([]);
+
+  async function fetchDrums() {
+    const data = await getDrums();
+    setDrums(data);
+  }
+
+  useEffect(() => {
+    fetchDrums();
+  }, []);
+
   return (
     <div className="App">
       
